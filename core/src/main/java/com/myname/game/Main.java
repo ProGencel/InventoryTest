ce7acbdaa655af09445692a2ea5db7c41a605508
Ahmet Efe Gençel <bloggeromeo@gmail.com>
2026-02-26 15:13:37 +0300

    initial commit

---
+++ core/src/main/java/com/myname/game/Main.java
@@ -0,0 +1,26 @@
+package com.myname.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
